5d94e90ea34d51c5cf07ea10a1bf3522c2d1bd73
Gabriele Gemmi <gabriele.gemmi@unive.it>
2022-05-10 13:57:44 +0200

    cleanup

--- Sim.py
+++ Sim.py
@@ -21,11 +21,9 @@ ox.config(use_cache=True, log_console=False)
 # set the folder for the cache
 cache = Cache(".cache")
 
-DSN = "postgresql://dbreader:dBReader23#@34.76.18.0/TrueNets"
-
 
 @cache.memoize()
-def read_data(base_folder: str, dataset: str) -> tuple[gpd.GeoDataFrame, nx.Graph, nx.MultiGraph]:
+def read_data(base_folder: str, dataset: str) -> tuple[gpd.GeoDataFrame | pd.Series, nx.Graph, nx.MultiGraph]:
     # Reads and join nodes and heights csv
     nodes = pd.read_csv(f"{base_folder}/vg/{dataset}/best_p.csv", sep=',', header=0, names=['id', 'x', 'y'], dtype=int).set_index('id', drop=False)
     nodes = gpd.GeoDataFrame(nodes, geometry=gpd.points_from_xy(nodes.x, nodes.y))
@@ -47,7 +45,6 @@ def read_data(base_folder: str, dataset: str) -> tuple[gpd.GeoDataFrame, nx.Grap
     nodes = nodes[nodes.households > 0]
     # Read graph from edgelist
     graph = nx.read_edgelist(
-        #f"{base_folder}/vg/{dataset}/best_p_intervisibility.edgelist",
         f"{base_folder}/vg/{dataset}/distance.edgelist",
         delimiter=" ",
         nodetype=int,
@@ -58,6 +55,7 @@ def read_data(base_folder: str, dataset: str) -> tuple[gpd.GeoDataFrame, nx.Grap
     
     #Get road graph from OSM
     if dataset == 'casciana terme':
+        #Casciana Terme changed name since last census, so OSM has a different name
         dataset='casciana terme lari'
     osmg = ox.graph_from_place(f'{dataset}, Italy')
     posmg = ox.project_graph(osmg, 'EPSG:3003')
@@ -70,7 +68,7 @@ class Simulator():
     def __init__(self, args):
         self.base_folder = args.base_folder
         self.dataset = args.dataset.lower()
-        engine = create_engine(DSN)
+        engine = create_engine(args.dsn)
         istat = gpd.read_postgis(f"""SELECT "PF1", geom, sez2011_tx::bigint as sez2011 FROM istat_agcom WHERE lower("COMUNE") = 'magliano in toscana'""", engine).set_index('sez2011', drop=False)
         self.random_seed = args.seed
         
@@ -85,9 +83,9 @@ class Simulator():
         with open('fiber_pop.json') as fr:
             self.fiber_pop = json.load(fr)[self.dataset]
 
-    def filter_nodes_households(self, pop_ratio):
-        self.pop_ratio = pop_ratio
-        target_households = int(self.total_households*self.pop_ratio)
+    def filter_nodes_households(self, subscribers_ratio):
+        self.subscribers_ratio = subscribers_ratio
+        target_households = int(self.total_households*self.subscribers_ratio)
         selected_nodes = self.nodes.sample(target_households, weights=self.nodes.households, replace=True).index
         subscriptions = Counter(selected_nodes)
         self.mynodes = self.nodes.loc[subscriptions.keys()]
@@ -133,13 +131,13 @@ class Simulator():
         plt.tight_layout()
         exit(0)
 
-    def generate_topologies(self, gw_strategy_name, topo_strategy_name):
-        base_dir = f'results/{self.dataset}_{(self.pop_ratio*100):.0f}_{self.cluster_size}/'
+    def generate_topologies(self):
+        base_dir = f'results/{self.dataset}_{(self.subscribers_ratio*100):.0f}_{self.cluster_size}/'
         os.makedirs(base_dir, exist_ok=True)
         TG = Topology(self.graph, self.mynodes, self.n_clusters)
         TG.extract_graph()
         TG.fiber_backhaul(self.osm_road, self.fiber_pop)
-        TG.save_graph(f'{base_dir}/{time.time()*10:.0f}_{self.random_seed}_{topo_strategy_name}_{gw_strategy_name}')
+        TG.save_graph(f'{base_dir}/{time.time()*10:.0f}_{self.random_seed}')
 
 
 def main():
@@ -147,20 +145,19 @@ def main():
     parser.add_argument('--base_folder', help='path of the directory containing the visibility graphs')
     parser.add_argument('-D', '--dataset', help='dataset')
     parser.add_argument('--cluster_size', type=int, action='append')
-    parser.add_argument('--pop_ratio', type=float, action='append')
-    parser.add_argument('--gw_strategy', type=str)
-    parser.add_argument('--topo_strategy', type=str)
+    parser.add_argument('--subscribers_ratio', type=float, action='append')
+    parser.add_argument('--dsn', type=str)
     parser.add_argument('--runs', type=int, default=1)
     parser.add_argument('--seed', help='random seed', default=int(random.random()*100))
     args = parser.parse_args()
     s = Simulator(args)
-    pbar = tqdm.tqdm(total=len(args.cluster_size)*len(args.pop_ratio)*args.runs)
+    pbar = tqdm.tqdm(total=len(args.cluster_size)*len(args.subscribers_ratio)*args.runs)
     for cs in args.cluster_size:
-        for pr in args.pop_ratio:
+        for sr in args.subscribers_ratio:
             for run in range(args.runs):
-                s.filter_nodes_households(pr)
+                s.filter_nodes_households(sr)
                 s.clusterize_metis(cs)
-                s.generate_topologies(args.gw_strategy, args.topo_strategy)
+                s.generate_topologies()
                 pbar.update(1)
     pbar.close()
             
